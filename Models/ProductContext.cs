69e8b26db63a85f3cf4a109f721cf226e09b9866
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2021-01-18 16:48:31 +0100

    Working on data access layer

---
+++ Models/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Wingtip_Toys.Models
+{
+    public class ProductContext : DbContext
+    {
+
+        public ProductContext() : base("WingtipToys")
+        { 
+        
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
