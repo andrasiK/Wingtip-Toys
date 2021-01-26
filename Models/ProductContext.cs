ed13102c99dfd35013745ef1a6a4d60d3e3982be
Kristóf Imre Andrási <andrasi.kristof@gmail.com>
2021-01-26 11:20:22 +0100

    working on shopping cart

--- Models/ProductContext.cs
+++ Models/ProductContext.cs
@@ -12,6 +12,7 @@ namespace Wingtip_Toys.Models
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartItem> ShoppingCartItems { get; set; }
 
 
     }
